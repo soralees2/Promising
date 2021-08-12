242ce7998702ebd8e445e5319b4b059fd6bc3ce0
Hayeon <cpgkrptdj@naver.com>
2021-08-12 17:45:32 +0900

    d

--- src/main/resources/static/js/common.js
+++ src/main/resources/static/js/common.js
@@ -1,10 +1,11 @@
 $(function(){
-	  $("#search").on("click",function(){
-        if($(".lookaround").css("display") == 'none'){
-            $(".lookaround").css("display","block");
-            $('body').css({"height":"100vh", "overflow":"hidden"});
-        };
-    });
+	$("#search").on("click",function(){
+		if($(".lookaround").css("display") == 'none'){
+			$(".lookaround").css("display","block");
+			$('body').css({"height":"100vh", "overflow":"hidden"});
+	    };
+	console.log("ddd");
+	});
 
     $("#search_close").on("click",function(){
         $(".lookaround").css("display","none");
@@ -14,4 +15,5 @@ $(function(){
     $("#searchBtn").on("click",function(){
     	
     });
+
 })
