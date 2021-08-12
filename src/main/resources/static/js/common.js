d1ae898d90ac5b8d42f07a276a24094f45c8055f
eelsoras2 <eelsoras2@gmail.com>
2021-08-12 17:30:30 +0900

    common.js add

---
+++ src/main/resources/static/js/common.js
@@ -0,0 +1,17 @@
+$(function(){
+	  $("#search").on("click",function(){
+        if($(".lookaround").css("display") == 'none'){
+            $(".lookaround").css("display","block");
+            $('body').css({"height":"100vh", "overflow":"hidden"});
+        };
+    });
+
+    $("#search_close").on("click",function(){
+        $(".lookaround").css("display","none");
+        $('body').css({"height":"auto", "overflow":"auto"});
+    });
+    
+    $("#searchBtn").on("click",function(){
+    	
+    });
+})
