88a02d5f062c04f9569a641e17b3c80d37341ce5
eelsoras2 <eelsoras2@gmail.com>
2021-08-15 23:48:53 +0900

    properties modify

--- src/main/java/com/promising/PromisingApplication.java
+++ src/main/java/com/promising/PromisingApplication.java
@@ -2,8 +2,10 @@ package com.promising;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/application-dev.properties")
 public class PromisingApplication {
 
 	public static void main(String[] args) {
