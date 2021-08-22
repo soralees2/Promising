9390e352b2d36d93ae4f3e3b3e4fe0791294e4c3
Gyuwon Jung <you@example.com>
2021-08-22 16:07:08 +0900

    qnaing

--- src/main/java/com/promising/controller/QnaController.java
+++ src/main/java/com/promising/controller/QnaController.java
@@ -1,16 +1,22 @@
 package com.promising.controller;
 
+import java.security.Principal;
 import java.util.List;
+
+import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.promising.config.SecurityConfig;
 import com.promising.repository.QnaRepository;
 import com.promising.vo.MemberVO;
 import com.promising.vo.QnaVO;
@@ -19,8 +25,28 @@ import com.promising.vo.QnaVO;
 @RequestMapping("/qna")
 public class QnaController {
 
+	
+	
 	@Autowired
-	private QnaRepository repo;
+	private QnaRepository qnaRepo;
+	
+	@Autowired
+	private SecurityConfig security;
+	
+	@Autowired
+	private HttpSession session;
+	
+	@RequestMapping(value="/home",method= {RequestMethod.GET})
+	public void qna(Model model, Principal principal) {
+		String writer =principal.getName();
+		System.out.println(writer);
+		 List<QnaVO> result = qnaRepo.selectQnaTome(writer);
+		 System.out.println(result);
+		 model.addAttribute("result", result);
+		}
+	
+	
+	
 	
 	@PostMapping("/{uname}")
 	public ResponseEntity<List<QnaVO>> addQna(@PathVariable("uname")String uname, @RequestBody QnaVO qvo){
@@ -29,12 +55,12 @@ public class QnaController {
 		MemberVO vo = new MemberVO();
 		vo.setUsername(uname);
 		qvo.setMember(vo);
-		repo.save(qvo);
+		qnaRepo.save(qvo);
 		
 		return new ResponseEntity<>(getListByMember(vo),HttpStatus.CREATED);
 	}
 	
 	private List<QnaVO> getListByMember(MemberVO vo) throws RuntimeException{
-		return repo.getQnaOfMember(vo);
+		return qnaRepo.getQnaOfMember(vo);
 	}
 }
