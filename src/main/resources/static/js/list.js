b7b40d63dc04413ab0c93f9d88be516ba86789d0
Hayeon <cpgkrptdj@naver.com>
2021-08-23 22:30:57 +0900

    dd

--- src/main/resources/static/js/list.js
+++ src/main/resources/static/js/list.js
@@ -1,5 +1,9 @@
 $(function(){
 	
+	let percent = new Array();
+	percent = $(".percent").val();
+	console.log("d" + percent);
+	
 	$("#seeMoreBtn").on("click",function(){
 		location.href="/project/list";
 	})
@@ -97,5 +101,38 @@ $(function(){
 		$("#f4").find("[name='keyword2']").val(keywordStr2);
 		$("#f4").find("[name='page']").val("1");
 		$("#f4").submit();
-	 });     
+	 });
+
+	$("#game").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=게임&keyword=게임";
+	})
+	
+	$("#perfume").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=향수&keyword=향수";
+	})
+	
+	$("#design").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=디자인&keyword=디자인";
+	})
+	
+	$("#knowledge").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=지식&keyword=지식";
+	})
+	
+	$("#trend").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=트렌드&keyword=트렌드";
+	})
+	
+	$("#publishing").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=출판&keyword=출판";
+	})
+	
+	$("#media").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=미디어&keyword=미디어";
+	})
+	
+	$("#animal").on("click",function(){
+		location.href="http://localhost/project/list?page=1&size=12&type=애완&keyword=애완";
+	}) 
+	    
 })
--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -18,35 +18,35 @@ $(function(){
 	
 	
 	$("#game").on("click",function(){
-		location.href="project/list?page=1&size=12&type=게임&keyword=게임";
+		location.href="http://localhost/project/list?page=1&size=12&type=게임&keyword=게임";
 	})
 	
 	$("#perfume").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=향수&keyword=향수";
 	})
 	
 	$("#design").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=디자인&keyword=디자인";
 	})
 	
 	$("#knowledge").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=지식&keyword=지식";
 	})
 	
 	$("#trend").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=트렌드&keyword=트렌드";
 	})
 	
 	$("#publishing").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=출판&keyword=출판";
 	})
 	
 	$("#media").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=미디어&keyword=미디어";
 	})
 	
 	$("#animal").on("click",function(){
-		
+		location.href="http://localhost/project/list?page=1&size=12&type=애완&keyword=애완";
 	})
 	
 	
